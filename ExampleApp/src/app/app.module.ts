c46459cdf52704bf58e2b5c18b7b2b5d0782b4a9
Sadig Najafov <sadig.najafov@gmail.com>
2018-09-11 10:49:55 +0200

    Components generated. Bootstrap installed

--- ExampleApp/src/app/app.module.ts
+++ ExampleApp/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ToolboxComponent } from './toolbox/toolbox.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      BreadcrumbComponent,
+      ToolboxComponent,
+      FooterComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ ExampleApp/src/app/toolbox/toolbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbox',
+  templateUrl: './toolbox.component.html',
+  styleUrls: ['./toolbox.component.css']
+})
+export class ToolboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
